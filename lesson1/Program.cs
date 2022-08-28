879565f55ed57baa5ef54f09fc93528c3e16abd5
EvgeniyaAfa <evgeniya_afanasieva@mail.ru>
2022-08-28 15:43:21 +0300

    Lectures.Lesson 1

---
+++ lesson1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача1. Hello world
+Console.WriteLine("Hello, World!");/*
+
+/*Задача2. Hello user
+Console.WriteLine("Input your name:   ");
+string username = Console.ReadLine();
+Console.WriteLine("Hello,   ");
+Console.WriteLine(username);*/
+
+/*Задача3. Hello special user
+Console.Write("Input your name:   ");
+string username = Console.ReadLine();
+    if(username.ToLower() == "evgeniya")
+    {
+       Console.Write("Hello,wow, Evgeniya ");
+    }
+    else
+    {
+       Console.Write("Hello,    ");
+       Console.WriteLine(username);
+    }
+/*
+/*Задача3. Найти наибольшее значение.
+
+int a = 5;
+int b = 9;
+int c = 10;
+int d = 50;
+
+int max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+Console.Write($"Max is {max} ");/*
